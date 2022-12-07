5617592439e5b49f6f496ee625e53d0386344a66
BigSev15 <palat008@umn.edu>
2022-12-06 19:22:47 -0600

    wrote breakfast and dinner models

---
+++ models/Dinner.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Dinner extends Model {}
+
+Dinner.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        calories: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        fats: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        carbs: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'dinner',
+      }
+);
+
+module.exports = Dinner;
