c2064fae135bf89d488716a972b6aed0b7522cf5
Kip Schetnan <kipdougschetnan@gmail.com>
2022-12-06 19:27:07 -0600

    creating models

--- models/Snacks.js
+++ models/Snacks.js
@@ -15,9 +15,33 @@ Snacks.init (
             type: DataTypes.STRING,
             allowNull: false,
         },
-        total_cals: {
+        calories: {
             type: DataTypes.INTEGER,
             allowNull: false,
         },
+        fats: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        carbs: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        modelName: 'snacks',
     }
 )
+
+module.exports = Snacks;
