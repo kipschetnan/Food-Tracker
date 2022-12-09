946a438b369248a6c53c5356928e19ed4895b722
Ksubi 1k <xksubi1x@gmail.com>
2022-12-08 18:58:08 -0600

    uploading html files

---
+++ home/script.js
@@ -0,0 +1,8 @@
+let ham = document.querySelector('.hamburger')
+
+let navbar = document.querySelector('.navbar')
+
+ham.addEventListener('click', () => {
+    navbar.classList.toggle('change')
+})
+
