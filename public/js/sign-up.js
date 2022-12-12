b522c9cd048d6c0a576621b5fcb0dacbcdf074f5
Kip Schetnan <kipdougschetnan@gmail.com>
2022-12-12 01:26:52 -0600

    fixed signup bug

--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -20,5 +20,5 @@ const signupFormHandler = async (event) => {
     }
 };
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('#button').addEventListener('click', signupFormHandler);
 
